23b1cdfffb3116b2bee92cd46943e026568e2399
Vijesh <vijeshkumarv@gmail.com>
2020-07-29 14:58:25 +0530

    Initial Submit (Mask detection, Face recognition completed.

---
+++ app/src/main/java/org/tensorflow/lite/examples/detection/Config.java
@@ -0,0 +1,7 @@
+package org.tensorflow.lite.examples.detection;
+
+public class Config {
+    public static final String EMAIL ="vedhikavijesh1@gmail.com"; //your-gmail-username
+    public static final String PASSWORD ="father@81"; //your-gmail-password
+    public static final String RECEPIENT_EMAIL = "vijeshhere@yahoo.com";//to mail.
+}
